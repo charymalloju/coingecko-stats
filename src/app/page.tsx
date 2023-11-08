7f6a48b293bcbad46ba10dc1af623dfdb7e817df
charymalloju <chary@vitwit.com>
2023-11-08 22:45:39 +0530

    second commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,18 @@
 import Image from 'next/image'
-
+import Script from 'next/script'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <script src="https://widgets.coingecko.com/coingecko-coin-price-static-headline-widget.js"></script>
-        <coingecko-coin-price-static-headline-widget coin-ids="celestia,cosmos,osmosis" currency="usd" locale="en"></coingecko-coin-price-static-headline-widget>
+        
+      <Script src="https://widgets.coingecko.com/coingecko-coin-price-static-headline-widget.js"></Script>
+        {/* @ts-ignore */}
+        <coingecko-coin-price-static-headline-widget coin-ids="celestia,cosmos,osmosis" currency="usd" locale="en" async></coingecko-coin-price-static-headline-widget>
 
-        <script src="https://widgets.coingecko.com/coingecko-coin-compare-chart-widget.js"></script>
-        <coingecko-coin-compare-chart-widget coin-ids="celestia,cosmos,osmosis" currency="usd" locale="en"></coingecko-coin-compare-chart-widget>
+        
+        <Script src="https://widgets.coingecko.com/coingecko-coin-compare-chart-widget.js"></Script>
+         {/* @ts-ignore */}
+        <coingecko-coin-compare-chart-widget coin-ids="celestia,cosmos,osmosis" currency="usd" locale="en" async></coingecko-coin-compare-chart-widget>
       </div>
     </main>
   )
