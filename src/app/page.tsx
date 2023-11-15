f6919860475f9f6e27f007d4f85a2740f80eb12c
chary <57086313+charymalloju@users.noreply.github.com>
2023-11-15 12:00:27 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,20 @@
+import { useEffect } from 'react';
+
 import Image from 'next/image'
 import Script from 'next/script'
 export default function Home() {
+  const refreshPage = () => {
+    window.location.reload();
+  };
+
+  // Use useEffect to set up the interval for refreshing
+  useEffect(() => {
+    const intervalId = setInterval(refreshPage, 10000);
+
+    // Clear the interval when the component is unmounted
+    return () => clearInterval(intervalId);
+  }, []); 
+  
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
