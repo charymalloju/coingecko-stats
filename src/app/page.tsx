66b7955d1d6e6b954bc60a74c7c8c27405d38327
chary <57086313+charymalloju@users.noreply.github.com>
2023-11-15 12:07:15 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
 
   // Use useEffect to set up the interval for refreshing
   useEffect(() => {
-    const intervalId = setInterval(refreshPage, 10000);
+    const intervalId = setInterval(refreshPage, 20000);
 
     // Clear the interval when the component is unmounted
     return () => clearInterval(intervalId);
